aacfa8c43486eb68fea3bc3ce836c009d5d5703b
Erik <erikolsson0@proton.me>
2026-05-07 16:35:31 +0000

    Clean up

--- src/main.c
+++ src/main.c
@@ -42,160 +42,181 @@ int main(int argc, char *argv[]) {
 
     // SDL_HideCursor();
 
-    SDL_Event event;
-    bool running = true;
-    int colorindex = 0; // First as default
-    SDL_Color chosenColor = {255, 0, 0};
-    SDL_Color chosenColorSaturation = {255, 255, 30};
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
-                running = false;
-            }
-        }
-
-        const bool *keyboardState = SDL_GetKeyboardState(NULL);
-        float cursorX, cursorY;
-        SDL_GetMouseState(&cursorX, &cursorY);
-
-        if (keyboardState[SDL_SCANCODE_1])
-            colorindex = 0;
-        else if (keyboardState[SDL_SCANCODE_2])
-            colorindex = 1;
-        else if (keyboardState[SDL_SCANCODE_3])
-            colorindex = 2;
-        else if (keyboardState[SDL_SCANCODE_4])
-            colorindex = 3;
-        if (keyboardState[SDL_SCANCODE_LSHIFT])
-            canva.colorIn(&canva, cursorX, cursorY,
-                          colorindex); // Paint if shift is pressed
-        if (keyboardState[SDL_SCANCODE_LCTRL] && keyboardState[SDL_SCANCODE_S]) {
-            canva.saveToPPM(&canva, "image.ppm");
-        }
-
-        // Exit if esc is pressed
-        if (keyboardState[SDL_SCANCODE_ESCAPE]) {
-            running = false;
-        }
-
-        SDL_Color drawColor = canva.palette[0];
-        SDL_Color targetColor;
-        SDL_SetRenderDrawColor(mainWindow.renderer, drawColor.r, drawColor.g, drawColor.b, 255);
-        for (int x = 0; x < canva.boxH; ++x) {
-            for (int y = 0; y < canva.boxV; ++y) {
-                targetColor = canva.grid[y * canva.boxH + x];
-
-                SDL_FRect rect = {.x = x * canva.boxSize,
-                                  .y = y * canva.boxSize,
-                                  .w = canva.boxSize,
-                                  .h = canva.boxSize};
-
-                if (targetColor.r == drawColor.r && targetColor.g == drawColor.g &&
-                    targetColor.b == drawColor.b) {
-                    SDL_RenderFillRect(mainWindow.renderer, &rect);
-                } else {
-                    drawColor = targetColor;
-                    SDL_SetRenderDrawColor(mainWindow.renderer, drawColor.r, drawColor.g,
-                                           drawColor.b, 255);
-                    SDL_RenderFillRect(mainWindow.renderer, &rect);
+    {
+        SDL_Event event;
+        bool running = true;
+        int colorindex = 0; // Index of color in palette that is used to draw (first as default)
+        SDL_Color chosenColor = {255, 0,
+                                 0}; // The color in the colorWindow that the user has picked
+        SDL_Color chosenColorSaturation = {
+            255, 255, 30}; // The color combined with the satureation that the user has picked
+        int height =
+            colorWindow.height /
+            3; // How high the bars will be (one bar each to color, saturation, and brightness)
+        int colorScale = 2; // This devides the length of the color bar with 2 (the bar will be too
+                            // big if it isn't shrinked) (it scales down)
+        int saturationAndBrightnessScale =
+            6 / colorScale; // Same thing as colorScale, but for saturation and brightness
+                            // bar (color bar is 6 times big!) (scales up)
+        while (running) {
+            while (SDL_PollEvent(&event)) {
+                if (event.type == SDL_EVENT_QUIT) {
+                    running = false;
                 }
             }
-        }
 
-        int x = 0, y = 0;
+            {
+                const bool *keyboardState = SDL_GetKeyboardState(NULL);
+                float cursorX, cursorY;
+                SDL_GetMouseState(&cursorX, &cursorY);
 
-        int count = 0;
-        while (y < PALETTE_SCREEN_HEIGHT_LEN) {
-            SDL_FRect rect = {.x = x * canva.boxSize,
-                              .y = y * canva.boxSize,
-                              .w = canva.boxSize,
-                              .h = canva.boxSize};
-            SDL_SetRenderDrawColor(paletteWindow.renderer, canva.palette[count].r,
-                                   canva.palette[count].g, canva.palette[count].b, 255);
-            SDL_RenderFillRect(paletteWindow.renderer, &rect);
+                if (keyboardState[SDL_SCANCODE_1])
+                    colorindex = 0;
+                else if (keyboardState[SDL_SCANCODE_2])
+                    colorindex = 1;
+                else if (keyboardState[SDL_SCANCODE_3])
+                    colorindex = 2;
+                else if (keyboardState[SDL_SCANCODE_4])
+                    colorindex = 3;
+                if (keyboardState[SDL_SCANCODE_LSHIFT])
+                    canva.colorIn(&canva, cursorX, cursorY,
+                                  colorindex); // Paint if shift is pressed
+                if (keyboardState[SDL_SCANCODE_LCTRL] && keyboardState[SDL_SCANCODE_S]) {
+                    canva.saveToPPM(&canva, "image.ppm");
+                }
 
-            count++;
-            x++;
-            if (x >= PALETTE_SCREEN_WIDTH_LEN) {
-                x = 0;
-                y++;
+                // Exit if esc is pressed
+                if (keyboardState[SDL_SCANCODE_ESCAPE]) {
+                    running = false;
+                }
             }
-        }
 
-        count = 0;
-        int height = colorWindow.height / 3; // The color section gets a third of the window
-        int colorChange = 2;
-        // From red to yellow
-        for (int color = 0; color < 256; color += colorChange) {
-            SDL_SetRenderDrawColor(colorWindow.renderer, 255, color, 0, 255);
-            SDL_RenderLine(colorWindow.renderer, count, 0, count,
-                           height); // Draw a vertical line with the right color
-            count++;
-        }
-        // From yellow to green
-        for (int color = 255; color >= 0; color -= colorChange) {
-            SDL_SetRenderDrawColor(colorWindow.renderer, color, 255, 0, 255);
-            SDL_RenderLine(colorWindow.renderer, count, 0, count,
-                           height); // Draw a vertical line with the right color
-            count++;
-        }
-        // From green to cyan
-        for (int color = 0; color < 256; color += colorChange) {
-            SDL_SetRenderDrawColor(colorWindow.renderer, 0, 255, color, 255);
-            SDL_RenderLine(colorWindow.renderer, count, 0, count, height);
-            count++;
-        }
-        // From cyan to blue
-        for (int color = 255; color >= 0; color -= colorChange) {
-            SDL_SetRenderDrawColor(colorWindow.renderer, 0, color, 255, 255);
-            SDL_RenderLine(colorWindow.renderer, count, 0, count, height);
-            count++;
-        }
-        // From blue to magenta
-        for (int color = 0; color < 256; color += colorChange) {
-            SDL_SetRenderDrawColor(colorWindow.renderer, color, 0, 255, 255);
-            SDL_RenderLine(colorWindow.renderer, count, 0, count, height);
-            count++;
-        }
-        // From magenta to red
-        for (int color = 255; color >= 0; color -= colorChange) {
-            SDL_SetRenderDrawColor(colorWindow.renderer, 255, 0, color, 255);
-            SDL_RenderLine(colorWindow.renderer, count, 0, count, height);
-            count++;
-        }
+            SDL_Color drawColor = canva.palette[0];
+            SDL_SetRenderDrawColor(mainWindow.renderer, drawColor.r, drawColor.g, drawColor.b, 255);
+            for (int x = 0; x < canva.boxH; ++x) {
+                for (int y = 0; y < canva.boxV; ++y) {
+                    SDL_Color boxColor = canva.grid[y * canva.boxH + x];
 
-        // Saturation (white to chosenColor)
-        count = 0;
-        for (int saturation = 0; saturation < 256; saturation++) {
-            SDL_SetRenderDrawColor(
-                colorWindow.renderer,
-                chosenColor.r + ((255 - chosenColor.r) * (255 - saturation) / 256),
-                chosenColor.g + ((255 - chosenColor.g) * (255 - saturation) / 256),
-                chosenColor.b + ((255 - chosenColor.b) * (255 - saturation) / 256), 255);
-            for (int i = 0; i < 6 / colorChange; ++i) {
-                SDL_RenderLine(colorWindow.renderer, count + i, height, count + i, height * 2);
+                    SDL_FRect rect = {.x = x * canva.boxSize,
+                                      .y = y * canva.boxSize,
+                                      .w = canva.boxSize,
+                                      .h = canva.boxSize};
+
+                    if (boxColor.r == drawColor.r && boxColor.g == drawColor.g &&
+                        boxColor.b == drawColor.b) {
+                        SDL_RenderFillRect(mainWindow.renderer, &rect);
+                    } else {
+                        drawColor =
+                            boxColor; // Change drawColor if it doesn't match with the boxColor
+                        SDL_SetRenderDrawColor(mainWindow.renderer, drawColor.r, drawColor.g,
+                                               drawColor.b, 255);
+                        SDL_RenderFillRect(mainWindow.renderer, &rect);
+                    }
+                }
             }
-            count += 6 / colorChange;
-        }
 
-        // Brightness (black to chosenColorSaturation)
-        count = 0;
-        for (int brightness = 0; brightness < 256; brightness++) {
-            SDL_SetRenderDrawColor(colorWindow.renderer,
-                                   (chosenColorSaturation.r * brightness / 255),
-                                   (chosenColorSaturation.g * brightness / 255),
-                                   (chosenColorSaturation.b * brightness / 255), 255);
-            for (int i = 0; i < 6 / colorChange; ++i) {
-                SDL_RenderLine(colorWindow.renderer, count + i, height * 2, count + i, height * 3);
+            {
+                int x = 0, y = 0; // X and Y pos of the box to be drawn in the paletteWindow
+                int count = 0;
+                while (y < PALETTE_SCREEN_HEIGHT_LEN) {
+                    SDL_FRect rect = {.x = x * canva.boxSize,
+                                      .y = y * canva.boxSize,
+                                      .w = canva.boxSize,
+                                      .h = canva.boxSize};
+                    SDL_SetRenderDrawColor(paletteWindow.renderer, canva.palette[count].r,
+                                           canva.palette[count].g, canva.palette[count].b, 255);
+                    SDL_RenderFillRect(paletteWindow.renderer, &rect);
+
+                    count++;
+                    x++;
+                    if (x >= PALETTE_SCREEN_WIDTH_LEN) {
+                        x = 0;
+                        y++;
+                    }
+                }
             }
-            count += 6 / colorChange;
+
+            {
+                int count = 0;
+                // From red to yellow
+                for (int color = 0; color < 256;
+                     color +=
+                     colorScale) { // Color increases by colorScale to make the bars shorter
+                    SDL_SetRenderDrawColor(colorWindow.renderer, 255, color, 0, 255);
+                    SDL_RenderLine(colorWindow.renderer, count, 0, count,
+                                   height); // Draw a vertical line with the right color
+                    count++;
+                }
+                // From yellow to green
+                for (int color = 255; color >= 0; color -= colorScale) {
+                    SDL_SetRenderDrawColor(colorWindow.renderer, color, 255, 0, 255);
+                    SDL_RenderLine(colorWindow.renderer, count, 0, count,
+                                   height); // Draw a vertical line with the right color
+                    count++;
+                }
+                // From green to cyan
+                for (int color = 0; color < 256; color += colorScale) {
+                    SDL_SetRenderDrawColor(colorWindow.renderer, 0, 255, color, 255);
+                    SDL_RenderLine(colorWindow.renderer, count, 0, count, height);
+                    count++;
+                }
+                // From cyan to blue
+                for (int color = 255; color >= 0; color -= colorScale) {
+                    SDL_SetRenderDrawColor(colorWindow.renderer, 0, color, 255, 255);
+                    SDL_RenderLine(colorWindow.renderer, count, 0, count, height);
+                    count++;
+                }
+                // From blue to magenta
+                for (int color = 0; color < 256; color += colorScale) {
+                    SDL_SetRenderDrawColor(colorWindow.renderer, color, 0, 255, 255);
+                    SDL_RenderLine(colorWindow.renderer, count, 0, count, height);
+                    count++;
+                }
+                // From magenta to red
+                for (int color = 255; color >= 0; color -= colorScale) {
+                    SDL_SetRenderDrawColor(colorWindow.renderer, 255, 0, color, 255);
+                    SDL_RenderLine(colorWindow.renderer, count, 0, count, height);
+                    count++;
+                }
+            }
+
+            {
+                // Saturation (white to chosenColor)
+                for (int saturation = 0; saturation < 256; saturation++) {
+                    SDL_SetRenderDrawColor(
+                        colorWindow.renderer,
+                        chosenColor.r + ((255 - chosenColor.r) * (255 - saturation) / 255),
+                        chosenColor.g + ((255 - chosenColor.g) * (255 - saturation) / 255),
+                        chosenColor.b + ((255 - chosenColor.b) * (255 - saturation) / 255), 255);
+                    for (int i = 0; i < saturationAndBrightnessScale; ++i) {
+                        SDL_RenderLine(colorWindow.renderer,
+                                       saturation * saturationAndBrightnessScale + i, height,
+                                       saturation * saturationAndBrightnessScale + i, height * 2);
+                    }
+                }
+            }
+
+            {
+                // Brightness (black to chosenColorSaturation)
+                for (int brightness = 0; brightness < 256; brightness++) {
+                    SDL_SetRenderDrawColor(colorWindow.renderer,
+                                           (chosenColorSaturation.r * brightness / 255),
+                                           (chosenColorSaturation.g * brightness / 255),
+                                           (chosenColorSaturation.b * brightness / 255), 255);
+                    for (int i = 0; i < saturationAndBrightnessScale; ++i) {
+                        SDL_RenderLine(colorWindow.renderer,
+                                       brightness * saturationAndBrightnessScale + i, height * 2,
+                                       brightness * saturationAndBrightnessScale + i, height * 3);
+                    }
+                }
+            }
+
+            SDL_RenderPresent(mainWindow.renderer);
+            SDL_RenderPresent(paletteWindow.renderer);
+            SDL_RenderPresent(colorWindow.renderer);
+
+            delay();
         }
-
-        SDL_RenderPresent(mainWindow.renderer);
-        SDL_RenderPresent(paletteWindow.renderer);
-        SDL_RenderPresent(colorWindow.renderer);
-
-        delay();
     }
 
     canva.destruct(&canva);
